dabf2c1a55190d37ff38222bec9f59f5f218ec75
debbykt <debbytampubolon24@gmail.com>
2026-05-12 07:31:10 -0700

    Unit Test

---
+++ AuthLibraryTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using AuthLibrary;
+using Xunit;
+
+namespace AuthLibraryTest
+{
+    public class UnitTest1
+    {
+        //Test Inputan Kosong
+        [Fact]
+        public void TestIsEmpty()
+        {
+            bool hasil = ValidasiInput.IsEmpty("");
+
+            Assert.True(hasil);
+        }
+
+        //Test Password Valid
+        [Fact]
+        public void TestPasswordValid()
+        {
+            bool hasil = ValidasiInput.IsPasswordValid("123456");
+
+            Assert.True(hasil);
+        }
+
+        //Test Username Valid
+        [Fact]
+        public void TestUsernameValid()
+        {
+            bool hasil = ValidasiInput.IsUsernameValid("debby123");
+
+            Assert.True(hasil);
+        }
+
+        //Test Nama Valid
+        [Fact]
+        public void TestNameValid()
+        {
+            bool hasil = ValidasiInput.IsNameValid("Debby");
+
+            Assert.True(hasil);
+        }
+    }
+}
